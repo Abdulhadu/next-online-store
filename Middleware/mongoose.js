d8b19b11001c70d99749c0774e25d3af76839ef7
hadi <hadi27767@gmail.com>
2023-07-21 12:29:23 -0700

    new debuging

--- Middleware/mongoose.js
+++ Middleware/mongoose.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
+mongoose.set('strictQuery', false);
+
 const connectdb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
   try {
-    mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
